0fc5fe797785336f08fcf23a37fc690a7acf438f
Vasiliy <white-aftor@yandex.ru>
2024-05-23 22:45:27 +0300

    remove unused file

--- packages/client/genAntdCss.tsx
+++
@@ -1,6 +0,0 @@
-import fs from 'fs'
-import { extractStyle } from '@ant-design/static-style-extract'
-
-const outputPath = './dist-ssr/antd.min.css'
-const css = extractStyle()
-fs.writeFileSync(outputPath, css)
